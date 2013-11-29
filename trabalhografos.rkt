92ac947a32f1479939c16163a056d46e4a812fdc
mhbackes <mhbackes@inf.ufrgs.br>
2013-11-29 20:07:57 -0200

    Várias coisas
    - Exemplos
    - Estruturas atualizadas
    - Funções separadas em principais, auxiliares e de impressão
    - Contratos atualizados
    - Coisas que o Cláudio tem que fazer
    - Função remove* excluida
    - Função dígrafo-tem-ciclo? atualizada
    - imprime-tem-ciclo-pseudo e imprime-tem-ciclo-di removidas
    - Função tem-ciclo? agora retorna booleanos
    - Espaço para as funções do Cláudio

--- trabalhografos.rkt
+++ trabalhografos.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname trabalhografos) (read-case-sensitive #t) (teachpacks ((lib "convert.ss" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "convert.ss" "teachpack" "htdp")))))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname trabalhografos) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ;; Nome do arquivo a ser lido
 (define src "grafo.txt")
 ;; Lê o arquivo de entrada e armazena em r, 
@@ -57,6 +57,83 @@
 ;   onde:
 ;   - caminho é um elemento de Caminho;
 ;   - lista-de-caminhos é um elemento de Lista-de-caminhos.
+
+;-----------------------------------------EXEMPLOS DE DADOS------------------------------------------
+
+; Listas de adjacência:
+(define la11 (list 0 1))
+(define la12 (list 1 0))
+(define la13 (list 2))
+
+(define la21 (list 0 1 1))
+(define la22 (list 1 0 2))
+(define la23 (list 2 1))
+
+; Grafos:
+(define g1 (list la11 la12 la13))
+(define g2 (list la21 la22 la23))
+
+;-----------------------------------------FUNÇÕES PRINCIPAIS-----------------------------------------
+
+; é-dígrafo?: Grafo Número -> Booleano
+; obj: Dados um grafo e seu tamanho, retorna true se o grafo é um dígrafo, caso o contrário,
+; retorna false.
+(define (é-dígrafo? grafo tamanho)
+  (cond
+    [(< tamanho 2) false]
+    [(= tamanho 2)
+     (aresta-direcionada? (first grafo) (second grafo))]
+    [else (or (ormap (lambda (x) (aresta-direcionada? (first grafo) x)) (rest grafo))
+              (é-dígrafo? (rest grafo) (- tamanho 1)))]))
+; Exemplos:
+; (é-dígrafo? g1 3)
+; deve retornar false
+; (é-dígrafo? g2 3)
+; deve retornar true
+
+; é-conexo?: Grafo Número -> Booleano
+; obj: Dados um grafo e seu tamanho, retorna true se o grafo for conexo, caso o
+; contrário retorna false.
+(define (é-conexo? grafo tamanho)
+  (cond
+    [(<= tamanho 1) true]
+    [else (teste-conectividade grafo (map first (cons (first (reverse grafo)) grafo)))]))
+
+; Exemplos:
+; (é-conexo? g1 3)
+; deve retornar false
+; (é-conexo? g2 3)
+; deve retornar true
+
+; ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO
+; DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PA
+; RA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLO
+; S DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUN
+; ÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁ
+; UDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE D
+; ETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - ESPAÇO PARA A FUNÇÃO DE DETECÇÃO DE CICLOS DO CLÁUDIO - E
+
+; acha-caminho: Nodo Nodo Grafo -> Lista-de-Caminhos
+; obj: Dados um nodo de origem e outro de destino, retorna
+; uma lista com todos os possíveis caminhos entre eles
+(define (acha-caminho origem destino grafo tamanho)
+  (cond
+    [(= origem destino) (cond
+                          [(é-dígrafo? grafo tamanho)
+                           (map (lambda (x) (cons origem x))
+                                (testa-caminho (vizinhos origem grafo) destino empty grafo))]
+                          [else (append (caminho-loop origem (vizinhos origem grafo))
+                                        (orig-orig-pseudo origem grafo))])]   
+    [else (testa-caminho (list origem) destino empty grafo)]))
+
+; distância: Nodo Nodo Grafo-> Número
+; obj: Dados um nodo de origem e outro de destino,
+; calcula sua distância
+(define (distância origem destino grafo)
+    (distância-larg (list origem) empty destino grafo))
+
+;-----------------------------------------FUNÇÕES AUXILIARES-----------------------------------------
+
 ; conta-n: Lista-de-nodos Nodo -> Número
 ; obj: Dados uma lista-de-números (lista) e um número(n), retorna o numero de
 ; ocorrências do número na lista.
@@ -70,24 +147,6 @@
   (cond
     [(= (conta-n (rest lista1)(first lista2)) (conta-n (rest lista2)(first lista1))) false]
     [else true]))
-    
-; é-dígrafo?: Grafo Número -> Booleano
-; obj: Dados um grafo e seu tamanho, retorna true se o grafo é um dígrafo, caso o contrário,
-; retorna false.
-(define (é-dígrafo? grafo tamanho)
-  (cond
-    [(< tamanho 2) false]
-    [(= tamanho 2)
-     (aresta-direcionada? (first grafo) (second grafo))]
-    [else (or (ormap (lambda (x) (aresta-direcionada? (first grafo) x)) (rest grafo))
-              (é-dígrafo? (rest grafo) (- tamanho 1)))]))
-
-(define (imprime-é-dígrafo? grafo tamanho)
-    (cond
-      [(é-dígrafo? grafo tamanho) (printf "O grafo é dígrafo.\n")]
-      [else (printf "O grafo não é dígrafo.\n")]))
-      
-(imprime-é-dígrafo? g n)
 
 ; vizinhos: Nodo Grafo -> Lista-de-números
 ; obj: Dados um grafo e um nodo retorna todos os nodos no grafo G
@@ -98,6 +157,15 @@
     [( equal? n (first (first G))) (rest (first G))]
     [else (vizinhos n (rest G))]))
 
+; teste-conectividade: Grafo Lista-de-nodos -> Booleano
+; obj: Dados um grafo e uma lista de nodos a serem testados, retorna true se
+; todos os nodos nodos estiverem contectados entre si, caso o contrário,
+; retorna false.
+(define (teste-conectividade grafo ldn)
+  (cond
+    [(empty? (rest ldn)) true]
+    [else (and (conectados-larg? (first ldn)(second ldn) grafo)
+               (teste-conectividade grafo (rest ldn)))]))
 
 ; conectados-larg? : Nodo Nodo Grafo -> Booleano
 ; obj: Dados dois nodos e um grafo, retorna true se existir um caminho
@@ -105,11 +173,10 @@
 (define (conectados-larg? a b G)
   (busca-larg (list a) (list ) b G))
 
-
 ; busca-larg : Lista-de-nodos Lista-de-nodos Nodo Grafo -> Booleano
 ; obj: Dados uma lista de nodos a percorrer (la), uma lista de nodos já percorridos (p)
-; um destino (b) e um grafo (G), retorna true se houver um caminho entre entre la e b,
-; caso o contrário retorna false.
+; um destino (b) e um grafo (G), retorna true se houver um caminho entre entre la e b
+; que não passe por p, caso o contrário retorna false.
 (define (busca-larg la p b G)
   (cond
     [(empty? la) false]
@@ -126,32 +193,7 @@
     [else (append (filter (lambda (x) (not (member x p)))
                           (vizinhos (first la) G))
                   (todos-vizinhos (rest la) p G))]))
-; é-conexo?: Grafo Número -> Booleano
-; obj: Dados um grafo e seu tamanho, retorna true se o grafo for conexo, caso o
-; contrário retorna false.
-(define (é-conexo? grafo tamanho)
-  (cond
-    [(<= tamanho 1) true]
-    [else (teste-conectividade grafo (map first (cons (first (reverse grafo)) grafo)))]))
 
-; teste-conectividade: Grafo Lista-de-nodos -> Booleano
-; obj: Dados um grafo e uma lista de nodos a serem testados, retorna true se
-; todos os nodos nodos estiverem contectados entre si, caso o contrário,
-; retorna false.
-(define (teste-conectividade grafo ldn)
-  (cond
-    [(empty? (rest ldn)) true]
-    [else (and (conectados-larg? (first ldn)(second ldn) grafo)
-               (teste-conectividade grafo (rest ldn)))]))
-
-; tem-loop?: Grafo -> Booleano
-; obj: Dado um grafo, retorna true se um de seus nodos
-; tem um loop , caso o contrário, retorna false
-(define (tem-loop? grafo)
-  (cond
-    [(empty? grafo) false]
-    [else (or (member (first (first grafo))(rest (first grafo)))
-              (tem-loop? (rest grafo)))]))
 ; testa-caminho: Lista-de-Nodos Nodo Caminho Grafo -> Lista-de-Caminhos
 ; obj: Dados uma lista de nodos adjacentes (la), um destino (destino),
 ; um caminho (caminho) e um grafo (grafo), retorna uma lista-de-caminhos
@@ -166,18 +208,72 @@
     [else (append (testa-caminho (vizinhos (first la) grafo) destino (append caminho (list (first la))) grafo)
                   (testa-caminho (rest la) destino caminho grafo))]))
 
-; acha-caminho: Nodo Nodo Grafo -> Lista-de-Caminhos
-; obj: Dados um nodo de origem e outro de destino, retorna
-; uma lista com todos os possíveis caminhos entre eles
-(define (acha-caminho origem destino grafo tamanho)
+; caminho-loop: Nodo Lista-de-nodos -> Lista-de-caminhos
+; obj: Dados um nodo e sua lista de vizinhos, retorna a lista de caminhos
+; com os loops desse nodo.
+(define (caminho-loop origem vizinhos)
   (cond
-    [(= origem destino) (cond
-                          [(é-dígrafo? grafo tamanho)(map (lambda (x) (cons origem x))(testa-caminho (vizinhos origem grafo) destino empty grafo))]
-                          [else (append (caminho-loop origem (vizinhos origem grafo))
-                                        (orig-orig-pseudo origem grafo))])]   
-    [else (testa-caminho (list origem) destino empty grafo)]))
+    [(empty? vizinhos) empty]
+    [(= origem (first vizinhos)) (cons (list origem origem) (caminho-loop origem (rest vizinhos)))]
+    [else (caminho-loop origem (rest vizinhos))]))
 
-; remove-todos: Nodo Lista-de-nodos ->Lista-de-nodos
+; orig-orig-pseudo: Nodo Grafo -> Lista-de-caminhos
+; obj: Dados um nodo e um pseudografo, retorna todos os caminhos que tem como
+; origem e destino esse nodo, exceto loops.
+(define (orig-orig-pseudo origem grafo)
+  (map (lambda (x) (cons origem x))
+       (foldr (lambda (x y)(append (testa-caminho (remove-todos origem (vizinhos x grafo)) origem (list x) grafo) y))
+              empty 
+              (remove-todos origem (vizinhos origem grafo)))))
+
+; distância-larg: Lista-de-nodos Lista-de-nodos Nodo Grafo -> Número
+; obj: Uma variação da função busca-larg que soma 1 a cada recursão.
+; Se houver um caminho entre la e b sem passar por p, retorna a menor
+; distância entre la e b, caso o contrário retorna infinito.
+(define (distância-larg la p b G)
+  (cond
+    [(empty? la) +inf.0]
+    [(member b la) 0]
+    [else (+ 1 (distância-larg (todos-vizinhos la p G) (append la p) b G))]))
+;----------------------------------------FUNÇÕES DE IMPRESSÃO----------------------------------------
+
+; imprime-é-dígrafo: Grafo Número -> Void
+; obj: Dados um grafo e seu tamanho, imprime
+; se ele é dígrafo ou não.
+(define (imprime-é-dígrafo? grafo tamanho)
+  (cond
+    [(é-dígrafo? grafo tamanho) (printf "O grafo é dígrafo.\n")]
+    [else (printf "O grafo não é dígrafo.\n")]))
+
+; imprime-é-dígrafo: Grafo Número -> Void
+; obj: Dados um grafo e seu tamanho, imprime
+; se ele é conexo ou não.
+(define (imprime-é-conexo? grafo tamanho)
+  (cond
+   [(é-conexo? grafo tamanho) (printf "O grafo é conexo.\n")]
+   [else (printf "O grafo não é conexo")]))
+
+; Chamada das funções de impressão:
+(imprime-é-dígrafo? g n)
+(imprime-é-conexo? g n)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; remove-todos: Nodo Lista-de-nodos -> Lista-de-nodos
 ; obj: Dados um nodo e uma lista-de-nodos, retira da lista
 ; os nodos iguais ao nodo.
 (define (remove-todos n lista)
@@ -185,66 +281,49 @@
     [(member n lista) (remove-todos n (remove n lista))]
     [else lista]))
 
-(define (caminho-loop origem vizinhos)
-  (cond
-    [(empty? vizinhos) empty]
-    [(= origem (first vizinhos)) (cons (list origem origem) (caminho-loop origem (rest vizinhos)))]
-    [else (caminho-loop origem (rest vizinhos))]))
 
-(define (orig-orig-pseudo origem grafo)
-  (map (lambda (x) (cons origem x))
-       (foldr (lambda (x y)(append (testa-caminho (remove-todos origem (vizinhos x grafo)) origem (list x) grafo) y))
-              empty 
-              (remove-todos origem (vizinhos origem grafo)))))
 
-; distância: Nodo Nodo -> Número
-; obj: Dados um nodo de origem e outro de destino,
-; calcula sua distância
-(define (distância origem destino grafo)
-    (distância-larg (list origem) empty destino grafo))
 
-(define (distância-larg la p b G)
+
+
+
+
+;;-----------------------------Coisas que o claudio tem que fazer--------------------
+
+; tem-loop?: Grafo -> Booleano
+; obj: Dado um grafo, retorna true se um de seus nodos
+; tem um loop , caso o contrário, retorna false
+(define (tem-loop? grafo)
   (cond
-    [(empty? la) +inf.0]
-    [(member b la) 0]
-    [else (+ 1 (distância-larg (todos-vizinhos la p G) (append la p) b G))]))
-;;-----------------------------Novos dados--------------------
-(define (remove* lista)
-  (cond
-    [(empty? lista) empty]
-    [(false? (first lista)) (remove* (rest lista))]
-    [else (cons (first lista) (remove* (rest lista)))]))
+    [(empty? grafo) false]
+    [else (or (member (first (first grafo))(rest (first grafo)))
+              (tem-loop? (rest grafo)))]))
 
 (define (dígrafo-tem-cíclo? grafo)
-    (cond
-      [(empty? (remove* (map (lambda (x) (busca-larg (rest x) empty (first x) grafo)) grafo))) false]
-      [else true]))
+     (ormap (lambda (x) (busca-larg (rest x) empty (first x) grafo)) grafo))  ;(apagar esse comentario) ficou muito mais simples assim
+
 (define (pseudo-ciclo la a grafo)
   (ormap (lambda (x) (busca-larg (remove a (todos-vizinhos (list x) empty grafo)) (list x) a grafo)) la))
 
 (define (pseudografo-tem-cíclo? grafo)
     (cond
       [(tem-loop? grafo) true] 
-      [else 
-      (ormap (lambda (x) (pseudo-ciclo (rest x) (first x) grafo)) grafo)]))
+      [else (ormap (lambda (x) (pseudo-ciclo (rest x) (first x) grafo)) grafo)]))
 
 
-(define (tem-ciclo? grafo tamanho)
+(define (tem-ciclo? grafo tamanho)         ; (apagar esse comentario) eu queria que essa função retornasse true ou false e nao void
   (cond
-    [(é-dígrafo? grafo tamanho) (imprime-tem-ciclo-di grafo)]
-    [else (imprime-tem-ciclo-pseudo grafo)]))
+    [(é-dígrafo? grafo tamanho) (dígrafo-tem-cíclo? grafo)]
+    [else (pseudografo-tem-cíclo? grafo)]))
 
-(define (imprime-tem-ciclo-pseudo grafo)
+(define (imprime-tem-ciclo? grafo tamanho)    ; (apagar esse comentario) colocar essa junto com as funções de impressão
     (cond
-      [(pseudografo-tem-cíclo? grafo) (printf "O grafo possui ciclo.\n")]
-      [else (printf "O grafo não possui ciclo.\n")]))
+      [(tem-ciclo? grafo tamanho) (printf "O grafo é cíclico.\n")]
+      [else (printf "O grafo é acíclico.\n")]))
 
-(define (imprime-tem-ciclo-di grafo)
-    (cond
-      [(dígrafo-tem-cíclo? grafo) (printf "O grafo possui ciclo.\n")]
-      [else (printf "O grafo não possui ciclo.\n")]))
 
-(tem-ciclo? g n)
+
+(imprime-tem-ciclo? g n)
       
       
 
